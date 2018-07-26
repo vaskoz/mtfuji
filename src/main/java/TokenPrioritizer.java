0114cdc92989e38887fe2363081f59cd5387d96a
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-07-25 21:39:09 -0600

    formatting nit

--- src/main/java/TokenPrioritizer.java
+++ src/main/java/TokenPrioritizer.java
@@ -1,15 +1,15 @@
-public interface TokenPrioritizer{
+public interface TokenPrioritizer {
 	/**
 	 * This method puts a token onto the priority queue.
 	 *
 	 * @param token the token that will be added to the priority queue
 	 */
-	void addToken( Token token );
+	void addToken(Token token);
 
 	/**
 	 * This method returns the token currently on the queue with
 	 * the highest priority. If multiple tokens have the same priority
-	 * it will return the first of those tokens to be added to the 
+	 * it will return the first of those tokens to be added to the
 	 * queue. May return null if no tokens remain on the queue.
 	 *
 	 * @return The token on the queue with the highest priority
